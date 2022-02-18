158b48bc941c8bad3398bef66bbf6b9f7d25e620
Ammar Jussa <ammar26497@gmail.com>
2022-02-18 18:33:13 +0500

    google analytics corrected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { lightTheme, darkTheme } from "../styles/theme";
 import { LIGHT_THEME, DARK_THEME } from "../constants";
 import { DefaultSeo } from "next-seo";
 import { MainSEO } from "../next-seo.config";
+import Script from "next/script";
+import * as gtag from "../gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState<string>(LIGHT_THEME);
@@ -32,6 +34,24 @@ function MyApp({ Component, pageProps }: AppProps) {
       <DefaultSeo {...MainSEO} />
       <ThemeProvider theme={theme === LIGHT_THEME ? lightTheme : darkTheme}>
         <GlobalStyles />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} theme={theme} changeTheme={changeTheme} />
       </ThemeProvider>
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ import Document, {
 import { ServerStyleSheet } from "styled-components";
 import config from "react-reveal/globals";
 import { Keywords } from "../constants";
-import Script from "next/script";
 
 config({ ssrFadeout: true });
 
@@ -48,23 +47,6 @@ export default class MyDocument extends Document {
             content={Keywords}
             data-shuvi-head="true"
           ></meta>
-          <link rel="manifest" href="/manifest.json" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          />
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-  `,
-            }}
-          />
         </Head>
         <body>
           <Main />
